eef7659065a3e46074b7a72edc55934c3ffd46a7
kj128 <jangkat@oregonstate.edu>
2023-02-02 21:37:16 -0800

    Added some error handling and removed commented out code

--- server/models/User.js
+++ server/models/User.js
@@ -1,25 +1,27 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const { default: isEmail } = require('validator/lib/isEmail');
 
 const UserSchema = mongoose.Schema({
     name: {
         type: String, 
-        required: true
+        required: [true, 'Please enter a valid name']
     }, 
     email: {
         type: String, 
-        required: true
+        required: [true, 'Please enter a valid email'],
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: isEmail, 
+            message: 'Invalid email', 
+            isAsync: false
+        }
     }, 
     password: {
         type: String,
-        required: true
+        required: [true, 'Please enter a valid password']
     },
-
-    /*
-    id: {
-        type: String
-    }
-    */
-
 })
 
 module.exports = User = mongoose.model('user', UserSchema);
