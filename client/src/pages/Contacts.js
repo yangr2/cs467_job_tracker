33f24f240100ff06172fb1addb36f8a995c211e9
yangr2 <71616956+yangr2@users.noreply.github.com>
2023-02-19 14:50:25 -0600

    Added frontend Contacts page
    - Added Contacts page in Frontend
    - Added "contacts" button into the navbar
    - Added navbar into contacts page

---
+++ client/src/pages/Contacts.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+const Contacts = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Contacts Page</h1>
+        </div>
+    );
+}
+export default Contacts;
