56854671ad32c448f94f28f612cc3c17d10ae85b
Stella Kim <62909702+nakim97@users.noreply.github.com>
2023-02-18 17:44:38 -0800

    finish frontend for Home Page & some styling changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,8 +16,8 @@ const Home = () => {
             </div>
 
             <div className="homeImgContainer">
-            {/* Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a> */}
-            <img src={home} alt='home' className="homeImg"/>
+              {/* Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a> */}
+              <img src={home} alt='home' className="homeImg"/>
             </div>
 
         </div>
